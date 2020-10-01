daba351a2cc4c78693b98b97e0e1c6f2731a6a39
Bartek Kryza <bkryza@gmail.com>
2020-10-01 19:19:07 +0200

    Added Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ],
     install_requires=['requests', 'six'],
     tests_require=['pytest', 'tox', 'tox-docker']
